1b08b807a2ab4857ab94a4c7f3aae271321e2109
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2023-02-07 16:17:14 -0800

    Added specifications for filter and contains methods in GymList class, and getCount method specification; added specifications for methods in ProfileList

--- src/main/model/GymList.java
+++ src/main/model/GymList.java
@@ -10,11 +10,21 @@ public interface GymList {
     @Override
     String toString();
 
+    // REQUIRES: name is not empty
+    // MODIFIES: this
+    // EFFECTS: returns a list of elements satisfying the given name
     GymList filter(String name);
 
+    // REQUIRES: name is not empty
+    // MODIFIES: this
+    // EFFECTS: returns true if list contains an element with the same name, otherwise returns false
     boolean contains(String name);
 
     // MODIFIES: this
     // EFFECTS: returns true if list is empty, otherwise returns false
     boolean isEmpty();
+
+    // MODIFIES: this
+    // EFFECTS: returns the number of elements in given list
+    int getCount();
 }
--- src/main/model/ProfileList.java
+++ src/main/model/ProfileList.java
@@ -3,7 +3,10 @@ package model;
 import java.util.ArrayList;
 
 public class ProfileList implements GymList {
+    private static final int DISPLAY_NUMBER_OF_ROWS = 10;
+
     private ArrayList<Profile> profiles;
+    private int count; // Count of elements in profiles
 
     // EFFECTS: Makes a new profile list with no profiles
     public ProfileList() {
@@ -11,24 +14,41 @@ public class ProfileList implements GymList {
     }
 
     // MODIFIES: this
-
+    // EFFECTS: returns the profile id and name of the first DISPLAY_NUMBER_OF_ROWS profiles in list,
+    //          and number of remaining rows in list
     public String toString() {
         return ""; // stub
     }
 
-    public GymList filter(String name) {
+    // MODIFIES: this
+    // EFFECTS: returns a list of profiles with the given name
+    public ProfileList filter(String name) {
         return null; // stub
     }
 
+    // MODIFIES: this
+    // EFFECTS: returns true if element with same name is in list
     public boolean contains(String name) {
         return false; // stub
     }
 
+    // MODIFIES: this
+    // EFFECTS: returns true if list is empty, otherwise returns false
     public boolean isEmpty() {
         return false;// stub
     }
 
-    public Profile getProfileList(int id) {
+    // MODIFIES: this
+    // EFFECTS: returns the profile with the given id
+    public Profile getProfile(int id) {
         return null; // stub
+    }
+
+    public ArrayList<Profile> getProfiles() {
+        return null; // stub
+    }
+
+    public int getCount() {
+        return 0; // stub
     }
 }
