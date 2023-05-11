afc177572facf489c9b918489994dce9078ca2df
Jeffrey Zhang <jjpzhang@students.cs.ubc.ca>
2023-05-10 22:11:35 -0700

    Added class level comment for MockRandomTest.

--- src/test/model/MockRandomTest.java
+++ src/test/model/MockRandomTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// Test class for MockRandom
 public class MockRandomTest {
     private MockRandom mockRandom;
 
